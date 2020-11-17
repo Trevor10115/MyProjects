041854800b93558b8f9bc5d450f3712c8c8315aa
Trevor Hatcher <Trevor10115@gmail.com>
2020-11-16 19:09:36 -0600

    test

--- Project1/src/P1/Main.java
+++ Project1/src/P1/Main.java
@@ -27,8 +27,7 @@ public class Main
 	     System.out.println(Min_Max.Min(file_1 ) + " Min Value");//prints min value
 	     System.out.println(Min_Max.Max(file_1) + " Max value");// prints max value
 	     System.out.println("Hello World!"); 
-	     System.out.println("Hello World!"); 
-	     System.out.println("Hello World!");
+	    
 	     
 	}
 }
