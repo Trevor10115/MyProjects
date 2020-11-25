92b4b4611658b4825a6c564561354bfee10d77e1
Trevor Hatcher <Trevor10115@gmail.com>
2020-11-25 13:01:28 -0600

    Started Project

--- Spell Checker & Knight's Tour/src/spellcheck/Main.java
+++ Spell Checker & Knight's Tour/src/spellcheck/Main.java
@@ -1,10 +1,19 @@
 package spellcheck;
+import java.io.*;
+import java.util.Scanner;
+
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception
+	{
+		
+		
+		for(int i = 0; i < FileMng.words.length; i++)
+		{
+			System.out.println(FileMng.words[i]);//prints contents of array 
+		}
 	}
-
+	
 }
---
+++ Spell Checker & Knight's Tour/src/spellcheck/spellChecks.java
@@ -0,0 +1,16 @@
+package spellcheck;
+
+
+public class spellChecks {
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
